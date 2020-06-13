b42dce9bf9fb1f70f6b9488a3bccef6776463dbf
R4356th <61620631+R4356th@users.noreply.github.com>
2020-06-13 19:37:30 +0600

    Update ScratchBlocks.php

--- ScratchBlocks.php
+++ ScratchBlocks.php
@@ -3,9 +3,8 @@
  * ScratchBlocks extension for MediaWiki
  * Renders <scratchblocks> tags to shiny scratch blocks
  *
- * Copyright 2013, Tim Radvan
- * MIT Licensed
- * http://opensource.org/licenses/MIT
+ * Copyright 2020, Radman Siddiki
+ * GNU General Public Version 3.0 Licensed 
  *
  * Includes scratchblocks v3
  * https://github.com/tjvr/scratchblocks
@@ -45,10 +44,10 @@ $wgResourceModules['ext.scratchBlocks'] = array(
 );
 
 $wgExtensionCredits['parserhook'][] = array(
-    'name' => "Scratchblocks",           // Name of extension - string
-    'description' => "This plugin takes text-based Scratch code and renders it in a graphical format.",    // Description of what the extension does - string. Omit in favour of descriptionmsg.
+    'name' => "Snapblocks",           // Name of extension - string
+    'description' => "This plugin takes text-based Snap! code and renders it in a graphical format.",    // Description of what the extension does - string. Omit in favour of descriptionmsg.
     'version' => 3.1,         // Version number of extension - number or string
-    'author' => ["ErnieParke","blob8108"],         // The extension author's name - string or array for multiple
+    'author' => ["R4356th"],         // The extension author's name - string or array for multiple
     'url' => "https://github.com/tjvr/wiki-scratchblocks",            // URL of extension (usually instructions) - string
-    'license-name' => "MIT",   // Short name of the license, links LICENSE or COPYING file if existing - string, added in 1.23.0
+    'license-name' => "GPL v3",   // Short name of the license
 );
