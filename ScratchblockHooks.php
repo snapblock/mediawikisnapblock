4fa9a4e8e40ae328beb960cf521de1458616ea13
R4356th <61620631+R4356th@users.noreply.github.com>
2020-06-14 18:56:44 +0600

    Update ScratchblockHooks.php

--- ScratchblockHooks.php
+++ ScratchblockHooks.php
@@ -1,11 +1,11 @@
 <?php
 class ScratchblockHook {
-	// Ouput HTML for <scratchblocks> tag
+	// Ouput HTML for <snapblocks> tag
 	
 	public static function sbParserInit (Parser $parser) {
 		// Register <scratchblocks> and <sb> tag
-		$parser->setHook('scratchblocks', array( "ScratchblockHook", 'sbRenderTag') );
-		$parser->setHook('sb', array( "ScratchblockHook", 'sbRenderInlineTag') );
+		$parser->setHook('snapblocks', array( "ScratchblockHook", 'sbRenderTag') );
+		$parser->setHook('sblocks', array( "ScratchblockHook", 'sbRenderInlineTag') );
 		//throw new Exception(var_dump($parser));
 		return true;
 	}
@@ -15,12 +15,12 @@ class ScratchblockHook {
 		$wgOut->addModules('ext.scratchBlocks');
 	}
 
-	// Output HTML for <scratchblocks> tag
+	// Output HTML for <snapblocks> tag
 	public static function sbRenderTag ($input, array $args, Parser $parser, PPFrame $frame) {
 		return '<pre class="blocks">' . htmlspecialchars($input) . '</pre>';
 	}
 
-	// Output HTML for inline <sb> tag
+	// Output HTML for inline <sblocks> tag
 	public static function sbRenderInlineTag ($input, array $args, Parser $parser, PPFrame $frame) {
 		return '<code class="blocks">' . htmlspecialchars($input) . '</code>';
 	}
